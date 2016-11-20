e4df5760cd74dcfc934a3b1d8ca5976b6ce24c21
JC Balcita <john.balcita@gmail.com>
2016-11-19 16:42:56 -0800

    example api requests

---
+++ cordcutter.js
@@ -0,0 +1,14 @@
+let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/show/17205/available_content`;
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', url, true);
+xhr.responseType = 'json';
+
+xhr.onload = function () {
+    if (xhr.readyState === xhr.DONE) {
+        if (xhr.status === 200) {
+            console.log(xhr.response);
+        }
+    }
+};
