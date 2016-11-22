baa8482fc352edad8784aed70c7987ac6344c193
JC Balcita <john.balcita@gmail.com>
2016-11-22 10:34:25 -0800

    basic communication between background and results.html

--- background.js
+++ background.js
@@ -1,17 +1,40 @@
+let searchString;
+
 chrome.contextMenus.create({
   id: "search-show",
   title: "Search by SHOW title",
   contexts:["selection"],
   onclick: function(e) {
-    console.log(encodeURIComponent(e.selectionText));
-  }
-});
+    chrome.tabs.create({ url: "results.html" });
+    searchString = e.selectionText;
+}});
 
 chrome.contextMenus.create({
   id: "search-movie",
   title: "Search by MOVIE title",
   contexts:["selection"],
   onclick: function(e) {
-   console.log(encodeURIComponent(e.selectionText));
+    cchrome.tabs.create({ url: "results.html" });
+    searchString = e.selectionText;
   }
 });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    // sent from another content script, intended for saving source
+    if(request.action === 'putSource') {
+        source = request.source;
+        chrome.tabs.create({ url: 'newtab.html' });
+    }
+    // sent from newtab-contentscript, to get the source
+    if(request.action === 'getResults') {
+
+        sendResponse({ source: searchString });
+    }
+});
+
+
+//
+// function printResults(searchString) {
+//   const results = document.getElementById("search-results");
+//   results.innerHTML = searchString;
+// }
---
+++ popup.js
@@ -0,0 +1,136 @@
+chrome.runtime.sendMessage({ action: 'getResults' }, function(response) {
+  const result = document.getElementById("search-results");
+  result.innerHTML = response.source;
+});
+
+// Search by show title
+// ********************
+// {Base API URL} /search/title/ {TRIPLE url encoded show name} / {"exact" or "fuzzy"}
+
+function searchForShow(searchString) {
+  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/search/title/${searchString}`;
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+
+  xhr.onload = function () {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        console.log(xhr.response);
+      }
+    }
+  };
+  xhr.send();
+}
+
+
+// Search by movie title
+// *********************
+// {Base API URL} /search/movie/title/ {TRIPLE url encoded show name} / {"exact" or "fuzzy"}
+function searchForMovie(searchString) {
+  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/search/movie/title/${searchString}`;
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+
+  xhr.onload = function () {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        console.log(xhr.response);
+        // do moar stuff here
+      }
+    }
+  };
+
+  xhr.send();
+}
+
+
+// Request information on show by ID
+// **********************************
+function getShowById(id) {
+  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/show/${id}/available_content`;
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+
+  xhr.onload = function () {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        console.log(xhr.response);
+        // do moar stuff here
+      }
+    }
+  };
+
+  xhr.send();
+}
+
+// Request information on movie by ID
+// **********************************
+function getMovieById(id) {
+  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/movie/${id}`;
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+
+  xhr.onload = function () {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        console.log(xhr.response);
+        // do moar stuff here
+      }
+    }
+  };
+
+  xhr.send();
+}
+
+// Request how many seasons a show has
+// *************************************************
+// Doesn't give much information -- probably only useful to find out
+// number of seasons... then creating a link for fetching each season.
+function getNumberOfSeasons(id) {
+  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/show/${id}/seasons`;
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+
+  xhr.onload = function () {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        console.log(xhr.response);
+        // do moar stuff here
+      }
+    }
+  };
+
+  xhr.send();
+}
+
+// Request information on a specific season of a show
+// **************************************************
+//{Base API URL} /show/ {id} /episodes/ {season} / {limit 1} / {limit 2} / {sources} / {platform} / {include links}
+function getSeasonInfo(id, season) {
+  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/show/${id}/episodes/${season}/1/25/all/all/true`;
+  let xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+
+  xhr.onload = function () {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        console.log(xhr.response);
+        // do moar stuff here
+      }
+    }
+  };
+
+  xhr.send();
+}
