7f63e4e83cf1b4af8c78ea67d9ab66c07fc91055
JC Balcita <john.balcita@gmail.com>
2016-11-27 14:43:36 -0800

    restyles, adds movie sources

--- popup.js
+++ popup.js
@@ -6,12 +6,14 @@ chrome.runtime.sendMessage({ action: 'getResults' }, function(response) {
   }
 });
 
+const hasLogo = ["Netflix", "Amazon Prime", "Hulu"]
+
 function processShowResults(results) {
   if (results.length > 0) {
     results.forEach(show => appendShowItem(show));
     console.log(results);
   } else {
-    document.getElementById("results").innerHTML = "No results found."
+    document.getElementById("results").textContent = "No results found."
   }
 }
 
@@ -20,105 +22,168 @@ function processMovieResults(results) {
     results.forEach(movie => appendMovieItem(movie));
     console.log(results);
   } else {
-    document.getElementById("results").innerHTML = "No results found."
+    document.getElementById("results").textContent = "No results found."
   }
 }
 
 function appendShowItem(show) {
   const resultEl = document.getElementById("initial-results-ul");
-  let listItem = newListItem();
   let image = document.createElement("img");
-  image.src = show.artwork_208x117;
-
-  listItem.dataset.id = show.id;
-  listItem.onclick = getShowById.bind(this, show.id);
-
-  listItem.appendChild(image);
-  listItem.appendChild(newSpan(show.title));
+    image.src = show.artwork_208x117;
+  let listItem = newListItem("initial");
+    listItem.dataset.id = show.id;
+    listItem.onclick = getShowById.bind(this, show.id);
+    listItem.appendChild(image);
+    listItem.appendChild(newSpan(show.title));
 
   resultEl.appendChild(listItem);
 }
 
 function appendMovieItem(movie) {
   const resultEl = document.getElementById("initial-results-ul");
-  let listItem = newListItem();
-  let image = document.createElement("img");
-  image.src = movie.poster_120x171;
+  let img = document.createElement("img");
+    img.src = movie.poster_120x171;
+  let listItem = newListItem("initial");
+    listItem.dataset.id = movie.id;
+    listItem.onclick = function() {
+      resultEl.textContent = "";
+      getMovieById(movie.id);
+    }
 
-  listItem.dataset.id = movie.id;
-  listItem.onclick = function() {
-    resultEl.innerHTML = "";
-    getMovieById(movie.id);
-  }
-
-  listItem.appendChild(image);
+  listItem.appendChild(img);
   listItem.appendChild(newSpan(`${movie.title} (${movie.release_year})`));
 
   resultEl.appendChild(listItem);
 }
 
-function newListItem() {
+function newListItem(type) {
   const li = document.createElement("li");
-  li.className = "list-item";
+  li.className = type === "initial" ? "list-item" : "source-item";
   return li;
 }
 
 function newSpan(content) {
   const span = document.createElement("span");
-  span.innerHTML = content;
+  span.textContent = content;
   return span;
+}
+
+function newSourceList(type) {
+  const sources = document.getElementById("sources")
+  const ul = document.createElement("ul");
+    ul.className = "source-list";
+  const h4 = document.createElement("h4");
+    h4.textContent = type;
+
+  ul.appendChild(h4);
+  sources.appendChild(ul);
+  return ul;
 }
 
 function displayMovieDetail(movie) {
   addMoviePoster(movie.poster_240x342);
-  addMovieOverview(movie.overview);
+  addMovieTitle(movie.title, movie.release_year);
+  // addFreeSources(movie.free_web_sources);
+  addSubSources(movie.subscription_web_sources);
+  addTVESources(movie.tv_everywhere_web_sources);
+  // addPurchaseSources(movie.purchase_web_sources);
 }
 
-function displayShowDetail(show) {
+function addFreeSources(freeSources) {
 
+}
+
+function addSubSources(subSources) {
+  if (subSources.length === 0) {
+    return null;
+  } else {
+    const ul = newSourceList("Subscription:");
+
+    subSources.forEach(source => {
+      addSource(source, ul);
+    });
+  }
+}
+
+function addTVESources(tveSources) {
+  if (tveSources.length === 0) {
+    return null;
+  } else {
+    const ul = newSourceList("Cable/Dish Login Required:");
+
+    tveSources.forEach(source => {
+      addSource(source, ul);
+    });
+  }
+}
+
+function addSource(source, sourceList) {
+  const li = newListItem("source");
+  const a = document.createElement("a");
+    a.href = source.link;
+    a.className = "source-link"
+    li.appendChild(a);
+
+    if (hasLogo.includes(source.display_name)) {
+      const img = document.createElement("img");
+        let sourceName = source.display_name.split(" ")[0].toLowerCase();
+        img.src = `assets/${sourceName}.png`;
+        img.className = "logo";
+        a.appendChild(img);
+    } else {
+      a.textContent = source.display_name
+    }
+  sourceList.appendChild(li);
+}
+
+
+// function displayShowDetail(show) {
+//
+// }
+
+function addMovieTitle(title, year) {
+  const detail = document.getElementById("item-detail");
+  const yearString = year.toString();
+  const h3 = document.createElement("h3");
+    h3.textContent = `${title}  (${yearString})`
+    h3.className = "movie-title"
+
+  detail.appendChild(h3);
 }
 
 function addMoviePoster(poster) {
   const detail = document.getElementById("item-detail");
   let img = document.createElement("img");
-  img.src = poster;
-  img.className = "movie-poster"
+    img.src = poster;
+    img.className = "movie-poster"
 
   detail.appendChild(img);
 }
 
-function addMovieOverview(overview) {
-  const detail = document.getElementById("item-detail");
-  let div = document.createElement("div");
-  div.className = "movie-overview"
-  div.innerHTML = overview;
 
-  detail.appendChild(div)
-}
-// *******************************
-// SEARCHES
+//
+// API CALLS, i.e. SEARCHES!
+//
 
 function newXHR(url) {
-  let xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.responseType = 'json';
 
-  return xhr
+  return xhr;
 }
 
 // Search by show title
-// ********************
 // {Base API URL} /search/title/ {TRIPLE url encoded show name} / {"exact" or "fuzzy"}
-
 function searchForShow(searchString) {
-  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/search/title/${searchString}`;
-  document.getElementById("loading").innerHTML = "Loading..."
+  const url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/search/title/${searchString}`;
+  document.getElementById("loading").textContent = "Loading..."
 
   let xhr = newXHR(url);
   xhr.onload = function () {
     if (xhr.readyState === xhr.DONE) {
       if (xhr.status === 200) {
-        document.getElementById("loading").innerHTML = ""
+        document.getElementById("loading").textContent = ""
         processShowResults(xhr.response.results);
       }
     }
@@ -132,14 +197,14 @@ function searchForShow(searchString) {
 // *********************
 // {Base API URL} /search/movie/title/ {TRIPLE url encoded show name} / {"exact" or "fuzzy"}
 function searchForMovie(searchString) {
-  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/search/movie/title/${searchString}`;
-  document.getElementById("loading").innerHTML = "Loading..."
+  const url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/search/movie/title/${searchString}`;
+  document.getElementById("loading").textContent = "Loading..."
 
-  let xhr = newXHR(url);
+  const xhr = newXHR(url);
   xhr.onload = function () {
     if (xhr.readyState === xhr.DONE) {
       if (xhr.status === 200) {
-        document.getElementById("loading").innerHTML = ""
+        document.getElementById("loading").textContent = ""
         processMovieResults(xhr.response.results);
       }
     }
@@ -148,13 +213,11 @@ function searchForMovie(searchString) {
   xhr.send();
 }
 
-
 // Request information on show by ID
-// **********************************
 function getShowById(id) {
-  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/show/${id}/available_content`;
+  const url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/show/${id}/available_content`;
 
-  let xhr = newXHR(url);
+  const xhr = newXHR(url);
   xhr.onload = function () {
     if (xhr.readyState === xhr.DONE) {
       if (xhr.status === 200) {
@@ -169,10 +232,9 @@ function getShowById(id) {
 }
 
 // Request information on movie by ID
-// **********************************
 function getMovieById(id) {
-  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/movie/${id}`;
-  let xhr = newXHR(url);
+  const url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/movie/${id}`;
+  const xhr = newXHR(url);
 
   xhr.onload = function () {
     if (xhr.readyState === xhr.DONE) {
@@ -187,12 +249,9 @@ function getMovieById(id) {
 }
 
 // Request how many seasons a show has
-// *************************************************
-// Doesn't give much information -- probably only useful to find out
-// number of seasons... then creating a link for fetching each season.
 function getNumberOfSeasons(id) {
-  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/show/${id}/seasons`;
-  let xhr = newXHR(url);
+  const url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/show/${id}/seasons`;
+  const xhr = newXHR(url);
 
   xhr.onload = function () {
     if (xhr.readyState === xhr.DONE) {
@@ -207,11 +266,10 @@ function getNumberOfSeasons(id) {
 }
 
 // Request information on a specific season of a show
-// **************************************************
 //{Base API URL} /show/ {id} /episodes/ {season} / {limit 1} / {limit 2} / {sources} / {platform} / {include links}
 function getSeasonInfo(id, season) {
-  let url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/show/${id}/episodes/${season}/1/25/all/all/true`;
-  let xhr = newXHR(url);
+  const url = `https://api-public.guidebox.com/v1.43/US/rKy1Hw9qICyXezey3TcAJ2uv0bWwQkmL/show/${id}/episodes/${season}/1/25/all/all/true`;
+  const xhr = newXHR(url);
 
   xhr.onload = function () {
     if (xhr.readyState === xhr.DONE) {
