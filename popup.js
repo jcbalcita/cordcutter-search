a06eef19a8ad7b16ed8b5045172f1e7840b75d67
JC Balcita <john.balcita@gmail.com>
2017-06-07 06:59:36 -0700

    use jquery in popup

--- popup.js
+++ popup.js
@@ -1,47 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const movieButton = document.getElementById("set-movie")
-  const showButton = document.getElementById("set-show")
-  const form = document.getElementById("search-form");
-  const input = document.getElementById("search-input");
+  const movieButton = $("#set-movie");
+  const showButton = $("#set-show");
+  const form = $("#search-form");
+  const input = $("#search-input");
 
   chrome.storage.local.get("type", data => {
     if (data.type === "movie") {
-      movieButton.classList.remove("lighten-4");
-      showButton.classList.add("lighten-4");
+      movieButton.removeClass("lighten-4");
+      showButton.addClass("lighten-4");
     } else if (data.type === "show") {
-      showButton.classList.remove("lighten-4");
-      movieButton.classList.add("lighten-4");
+      showButton.removeClass("lighten-4");
+      movieButton.addClass("lighten-4");
     } else {
       chrome.storage.local.set({ type: "movie" });
-      movieButton.classList.remove("lighten-4");
+      movieButton.removeClass("lighten-4");
     }
   });
 
-  movieButton.onclick = function() {
+  movieButton.onclick = () => {
     chrome.storage.local.set({ type: "movie" }, () => {
-      this.classList.remove("lighten-4");
-      showButton.classList.add("lighten-4");
+      this.removeClass("lighten-4");
+      showButton.addClass("lighten-4");
       input.focus();
     });
   }
 
-  showButton.onclick = function() {
+  showButton.onclick = () => {
     chrome.storage.local.set({ type: "show" }, () => {
-      this.classList.remove("lighten-4");
-      movieButton.classList.add("lighten-4");
+      this.removeClass("lighten-4");
+      movieButton.addClass("lighten-4");
       input.focus();
     });
   }
 
   form.addEventListener("submit", e => {
     e.preventDefault();
-    const errors = document.getElementById("errors")
+    const errors = $("#errors");
     if (input.value) {
-      errors.textContent = ""
+      errors.text("");
       chrome.storage.local.set({ search: encodeURIComponent(input.value.slice(0, 75)) },
       () => chrome.tabs.create({ url: "results.html" }));
     } else {
-      errors.textContent = "Search field can't be blank."
+      errors.text("Search field can't be blank.");
     }
   });
 });
