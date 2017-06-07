ebeb1c538ba3faa2473e5e1014aa5d73201f040c
JC Balcita <john.balcita@gmail.com>
2017-06-07 06:58:09 -0700

    ES6 style functional declaration

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.contextMenus.create({
   title: "Search by SHOW title",
   contexts:["selection"],
-  onclick: function(e) {
+  onclick: e => {
     chrome.storage.local.set({ search: encodeURIComponent(e.selectionText.slice(0, 75)), type: "show" },
     () => chrome.tabs.create({ url: "results.html" }));
   }
@@ -10,7 +10,7 @@ chrome.contextMenus.create({
 chrome.contextMenus.create({
   title: "Search by MOVIE title",
   contexts:["selection"],
-  onclick: function(e) {
+  onclick: e => {
     chrome.storage.local.set({ search: encodeURIComponent(e.selectionText.slice(0, 75)), type: "movie" },
     () => chrome.tabs.create({ url: "results.html" }));
   }
